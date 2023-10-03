46cc89652b51dacf472d16177dd53e288ddd0602
unknown <deo_singiza@hotmail.com>
2023-10-03 05:36:05 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["avatars.googleusercontent.com"],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://api.grafbase.com/graphql" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+          // Additional custom headers
+          { key: "Content-Type", value: "text/event-stream" },
+          { key: "Cache-Control", value: "no-cache" },
+          { key: "Connection", value: "keep-alive" },
+          { key: "X-Accel-Buffering", value: "no" },
+        ],
+      },
+    ];
   },
 };
 
