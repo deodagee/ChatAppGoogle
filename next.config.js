120c2d2b815aca111ddfd8e71692ebc2d8fabccc
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-10-03 05:56:15 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,11 +13,6 @@ const nextConfig = {
           { key: "Access-Control-Allow-Origin", value: "https://api.grafbase.com/graphql" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
-          // Additional custom headers
-          { key: "Content-Type", value: "text/event-stream" },
-          { key: "Cache-Control", value: "no-cache" },
-          { key: "Connection", value: "keep-alive" },
-          { key: "X-Accel-Buffering", value: "no" },
         ],
       },
     ];
